073a01898091034d0b54ece03b97106a1026c768
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-07-14 14:27:56 -0300

    feat: adding functions to check if name and surname are valid or not

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,10 @@
+<?php
+
+require 'usuario.php';
+
+$usuario = new \Alura\Usuario($_POST['nome']);
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,12 +16,14 @@
 </head>
 <body>
 
+
+
 <div class="mx-5 my-5">
 <h1>Cadastro feito com sucesso.</h1>
 <p>Seguem os dados de sua conta:</p>
 <ul class="list-group">
-    <li class="list-group-item">Primeiro nome: </li>
-    <li class="list-group-item">Sobrenome: </li>
+    <li class="list-group-item">Primeiro nome: <?php echo $usuario-getNome(); ?></li>
+    <li class="list-group-item">Sobrenome:<?php echo $usuario-getSobrenome(); ?> </li>
     <li class="list-group-item">Usuário: </li>
     <li class="list-group-item">Senha: </li>
     <li class="list-group-item">Telefone: </li>
